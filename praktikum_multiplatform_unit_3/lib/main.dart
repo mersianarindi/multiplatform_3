5a9c37c91f955b2921d40dcd00d8746f02ace58b
mersianarindi <mersianarindi84@gmail.com>
2026-03-16 20:47:22 +0700

    update proyek unit 3

--- praktikum_multiplatform_unit_3/lib/main.dart
+++ praktikum_multiplatform_unit_3/lib/main.dart
@@ -20,13 +20,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Praktikum Layout Flutter')),
-      body:Column(
-mainAxisAlignment: MainAxisAlignment.center,
-crossAxisAlignment: CrossAxisAlignment.end,
+      body:  ListView(
 children: <Widget>[
-Container(color: Colors.red, height: 100, width: 100),
-Container(color: Colors.green, height: 50, width: 50),
-Container(color: Colors.blue, height: 80, width: 80),
+Container(color: Colors.red, height: 500, width: 100),
+Container(color: Colors.green, height: 500, width: 100),
+Container(color: Colors.blue, height: 500, width: 100),
 ],
 )
       );
