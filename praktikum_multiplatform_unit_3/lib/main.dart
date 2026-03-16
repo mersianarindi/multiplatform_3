e4749ccdab1e176a098cc8ee171af29f461ae0e2
mersianarindi <mersianarindi84@gmail.com>
2026-03-16 20:29:36 +0700

    update proyek unit 3

---
+++ praktikum_multiplatform_unit_3/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout Praktikum',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Praktikum Layout Flutter')),
+      body:Row(
+mainAxisAlignment: MainAxisAlignment.center,
+children: <Widget>[
+Container(color: Colors.red, height: 100, width: 100),
+Container(color: Colors.green, height: 50, width: 100),
+Container(color: Colors.blue, height: 80, width: 100),
+],
+)
+      );
+    }
+  }
